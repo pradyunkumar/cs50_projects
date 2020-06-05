631842f9caacc8f5daef87a5695daa7cc57bba91
Pradyun <pradyunkumar03@gmail.com>
2020-06-04 20:12:30 -0700

    cs50 AI Project 1a

---
+++ puzzle.py
@@ -0,0 +1,71 @@
+from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+# Puzzle 0
+# A says "I am both a knight and a knave."
+knowledge0 = And(
+    Implication(AKnight, And(AKnight, AKnave)), Implication(AKnave, Not(And(AKnight, AKnave))),
+    Biconditional(AKnight, Not(AKnave)), Biconditional(AKnave, Not(AKnight))
+)
+
+# Puzzle 1
+# A says "We are both knaves."
+# B says nothing.
+knowledge1 = And(
+    Implication(AKnave, Not(And(AKnave, BKnave))), Implication(AKnight, And(AKnave, BKnave)),
+    Biconditional(AKnight, Not(AKnave)), Biconditional(BKnave, Not(BKnight))
+)
+
+# Puzzle 2
+# A says "We are the same kind."
+# B says "We are of different kinds."
+knowledge2 = And(
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight)))),
+    Biconditional(AKnight, Not(AKnave)), Biconditional(BKnight, Not(BKnave))
+)
+
+# Puzzle 3
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+knowledge3 = And(
+    Biconditional(AKnave, Not(AKnight)), Biconditional(BKnave, Not(BKnight)), Biconditional(CKnave, Not(CKnight)),
+    Implication(AKnight, Or(AKnight, AKnave)), Implication(AKnave, Not(Or(AKnight, AKnave))),
+    Implication(AKnight, BKnave), Implication(AKnave, Not(BKnave)),
+    Implication(BKnight, CKnave), Implication(BKnave, Not(CKnave)),
+    Implication(CKnight, AKnight), Implication(CKnave, Not(AKnight))
+)
+
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+
+if __name__ == "__main__":
+    main()
